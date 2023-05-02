7e3a57599402ea2cbd3e5a992b529a8cbeacf070
Julesz1 <132320189+Julesz1@users.noreply.github.com>
2023-05-02 12:10:43 -0300

    Update desafio4.cpp

--- desafio4.cpp
+++ desafio4.cpp
@@ -20,15 +20,14 @@ int main() {
     double posicao_carro = v_carro * tempo; 
     double posicao_caminhao = v_caminhao * tempo - t_pedagio * distancia/2.0; 
 
-    //verificando a posição que eles estão em km quando se encontram, eu verifico quantos km cada um está de Ribeirão:
-    double carro_rib = 100 - posicao_carro;
+     //verificando a posição que o caminhão está em km quando se encontram, eu verifico quantos km ele está de ribeirão (no caso do carro, a km que ele está de ribeirão é a km de posição dele pois ele está saindo de ribeirão rumo a franca)
     double caminhao_rib = 100 - posicao_caminhao;
 
-    //aqui eu faço a validação de qual posicionamento está mais proximo de ribeirão.  
-    if (carro_rib > caminhao_rib) {
-        cout << "O caminhão está mais próximo de Ribeirão Preto." << endl;
-    } else{
+     //aqui eu faço a validação de qual posicionamento está mais proximo de ribeirão. Se o km que o carro percorreu saindo de RP for menor que o km que o caminhao tem que percorrer para chegar até RP, o carro estará mais proximo. Caso contrario, o caminhao estará mais proximo. 
+    if (posicao_carro < caminhao_rib) {
         cout << "O carro está mais próximo de Ribeirão Preto." << endl;
+    } else{
+        cout << "O caminhão está mais próximo de Ribeirão Preto." << endl;
     }
 
     return 0;
